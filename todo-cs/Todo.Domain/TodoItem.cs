6cd3e938c2b9fdaa34bc70506badac0bcb87707a
Kashif Soofi <kashif.soofi+git@gmail.com>
2025-10-25 21:56:39 +0100

    Blazor todo app (#2)
    * Create blazorwasm project
    * Remove Central Package Management
    * Blazor Wasm application to manage simple Todo Items

--- todo-cs/Todo.Domain/TodoItem.cs
+++ todo-cs/Todo.Domain/TodoItem.cs
@@ -5,5 +5,10 @@ public record TodoItem(
     string Name,
     bool IsComplete)
 {
+    public TodoItem()
+        : this(Guid.NewGuid().ToString("N")[..5], string.Empty, false)
+    {
+    }
+
     public bool IsComplete { get; set; } = IsComplete;
 }
